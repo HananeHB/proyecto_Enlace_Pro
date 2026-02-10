c5940aefe6d63a9c5aaa33a04378f6506c308ba1
Nuhaila <nuhailaassaid16@gmail.com>
2026-02-09 18:41:10 -0600

    agrego la anotacion @schema en el idiomaResponse

--- src/main/java/es/etg/daw/dawes/java/rest/enlacePro/enlacePro/alumnos/infraestructure/web/dto/idioma/IdiomaResponse.java
+++ src/main/java/es/etg/daw/dawes/java/rest/enlacePro/enlacePro/alumnos/infraestructure/web/dto/idioma/IdiomaResponse.java
@@ -2,5 +2,13 @@ package es.etg.daw.dawes.java.rest.enlacePro.enlacePro.alumnos.infraestructure.w
 
 import java.time.LocalDateTime;
 
-public record IdiomaResponse(int id, String nombre, LocalDateTime createdAt) {
-} 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record IdiomaResponse(
+        @Schema(description = "ID único del idioma", example = "23") 
+        int id,
+        @Schema(description = "Nombre del idioma", example = "Inglés") 
+        String nombre,
+        @Schema(description = "Fecha matriculación del idioma", example = "2024-05-20") 
+        LocalDateTime createdAt) {
+}
