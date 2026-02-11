95be5dc80d6b3df485ff43730765aeff9c1eb735
HananeHB <hananebh146@gmail.com>
2026-02-10 20:52:25 -0600

    arreglo realizado

--- src/main/java/es/etg/daw/dawes/java/rest/enlacePro/enlacePro/alumnos/application/usecase/idioma/DeleteIdiomaUseCase.java
+++ src/main/java/es/etg/daw/dawes/java/rest/enlacePro/enlacePro/alumnos/application/usecase/idioma/DeleteIdiomaUseCase.java
@@ -1,8 +1,6 @@
 package es.etg.daw.dawes.java.rest.enlacePro.enlacePro.alumnos.application.usecase.idioma;
 
 import es.etg.daw.dawes.java.rest.enlacePro.enlacePro.alumnos.application.command.idioma.DeleteIdiomaCommand;
-import es.etg.daw.dawes.java.rest.enlacePro.enlacePro.alumnos.domain.error.IdiomaNotFoundException;
-import es.etg.daw.dawes.java.rest.enlacePro.enlacePro.alumnos.domain.model.id.IdiomaId;
 import es.etg.daw.dawes.java.rest.enlacePro.enlacePro.alumnos.domain.repository.IdiomaRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -11,6 +9,6 @@ public class DeleteIdiomaUseCase {
     private final IdiomaRepository idiomaRepository;
 
     public void delete(DeleteIdiomaCommand command){
-        idiomaRepository.deleteById(idiomaId);
+        idiomaRepository.deleteById(command.id());
     }
 }
