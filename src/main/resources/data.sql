6af1d1b420c5cc06459db47f05f5bcd53d5c9457
Nuhaila <nuhailaassaid16@gmail.com>
2026-02-12 18:46:08 -0600

    agrego latabla de usuarios en el archivo schema.sql y dos usuarios en data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,5 +14,9 @@ INSERT INTO ALUMNOS (nombre, apellidos, email, numero_telefono, fecha_creacion, 
 ('Andrei', 'Ionescu', 'andrei.ionescu@example.com', '600890123', CURRENT_TIMESTAMP, 'Avenida Dacia 8', 2),
 ('Zhang', 'Hua', 'zhang.hua@example.com', '600901234', CURRENT_TIMESTAMP, 'Calle del Bambú 33', 3);
 
+INSERT INTO USUARIOS (id, firstname, lastname, email, password, role ) VALUES 
+(1,'user', 'user', 'user@a.com', '$2a$12$PB6GwcJCu44g/T895VObduyyMGsX.Rl/QN0kU/IY0l9HOPokGJBpe', 'USER'),
+(2,'admin', 'admin', 'admin@a.com', '$2a$12$crH/Tuxn/3in7inhgLzoiOTfaXm2sLQy5alm0pwlBXu2z./MzwTMW', 'ADMIN');
+
 ALTER TABLE ALUMNOS ALTER COLUMN id RESTART WITH 10;
 ALTER TABLE IDIOMAS ALTER COLUMN id RESTART WITH 4;
