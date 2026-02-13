4825d62df70ea35a29180da63f61b68801938559
HananeHB <hananebh146@gmail.com>
2026-02-13 22:38:43 +0100

    Agregar JwtFilter que intercepta peticiones, valida el token JWT y carga el usuario con su rol en el SecurityContext

---
+++ src/main/java/es/etg/daw/dawes/java/rest/enlacePro/enlacePro/auth/infraestructure/security/JwtFilter.java
@@ -0,0 +1,61 @@
+package es.etg.daw.dawes.java.rest.enlacePro.enlacePro.auth.infraestructure.security;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtFilter extends OncePerRequestFilter{
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        //obtiene el header Authorization de la petición
+        String header = request.getHeader("Authorization");
+
+        
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+
+            try {
+                // Valida el token usando JwtUtil
+                Jws<Claims> claims = JwtUtil.validarToken(token);
+                // Extrae el email (subject del token)
+                String email = claims.getBody().getSubject();
+
+                 // Extrae el rol almacenado como claim
+                String role= claims.getBody().get("role",String.class);
+                UsernamePasswordAuthenticationToken authentication=
+                                new UsernamePasswordAuthenticationToken(
+                                    email,
+                                    null,
+                                    Collections.singletonList(
+                                        new SimpleGrantedAuthority("ROLE_"+role))
+                                    );
+                /**Esto permite guardar la autenticación en el contexto de seguridad. 
+                 * Permite que Spring apligue hasRole() o hasAnyRole()
+                 */
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (Exception e) {
+                /**
+                 * Si el token es inválido, expiró o fue manipulado no se establece 
+                 * autenticación y la petición seguirá sin usuario autenticado
+                 */
+            }
+        }
+        //continúa con el siguiente filtro de la cadena
+        filterChain.doFilter(request, response);
+    }
+    
+}
---
+++ src/main/java/es/etg/daw/dawes/java/rest/enlacePro/enlacePro/auth/infraestructure/web/rest/AuthController.java
@@ -0,0 +1,23 @@
+package es.etg.daw.dawes.java.rest.enlacePro.enlacePro.auth.infraestructure.web.rest;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.etg.daw.dawes.java.rest.enlacePro.enlacePro.auth.application.AuthService;
+import es.etg.daw.dawes.java.rest.enlacePro.enlacePro.auth.infraestructure.web.dto.LoginRequest;
+import es.etg.daw.dawes.java.rest.enlacePro.enlacePro.auth.infraestructure.web.dto.LoginResponse;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody LoginRequest request){
+        String tocken=authService.login(request.email(), request.password());
+        return new LoginResponse(tocken);
+    }
+}
