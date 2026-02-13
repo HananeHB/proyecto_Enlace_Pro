e0db76621cea2d26c4b18b1213d42a7b3638faed
Nuhaila <nuhailaassaid16@gmail.com>
2026-02-12 18:37:36 -0600

    agrego el codigo de la clase UserEntity y la clase UserRepository con codigo

--- src/main/java/es/etg/daw/dawes/java/rest/enlacePro/enlacePro/auth/infraestructure/db/jpa/entity/UserEntity.java
+++ src/main/java/es/etg/daw/dawes/java/rest/enlacePro/enlacePro/auth/infraestructure/db/jpa/entity/UserEntity.java
@@ -1,5 +1,33 @@
 package es.etg.daw.dawes.java.rest.enlacePro.enlacePro.auth.infraestructure.db.jpa.entity;
 
+import es.etg.daw.dawes.java.rest.enlacePro.enlacePro.auth.domain.Rol;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "USUARIOS")
 public class UserEntity {
     
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Rol rol;
 }
