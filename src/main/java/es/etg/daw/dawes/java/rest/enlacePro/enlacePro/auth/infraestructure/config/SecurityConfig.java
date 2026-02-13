e03e745f1de6c35b08e0b24e4a8d039a0645dd4b
HananeHB <hananebh146@gmail.com>
2026-02-13 20:50:19 +0100

    activada la autenticación y pequeños cambios(quitado el harcode)

--- src/main/java/es/etg/daw/dawes/java/rest/enlacePro/enlacePro/auth/infraestructure/config/SecurityConfig.java
+++ src/main/java/es/etg/daw/dawes/java/rest/enlacePro/enlacePro/auth/infraestructure/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,6 +18,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import es.etg.daw.dawes.java.rest.enlacePro.enlacePro.auth.domain.Rol;
 import es.etg.daw.dawes.java.rest.enlacePro.enlacePro.auth.infraestructure.db.jpa.repository.UserEntityRepository;
 import es.etg.daw.dawes.java.rest.enlacePro.enlacePro.auth.infraestructure.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +28,12 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
     private final UserEntityRepository repository;
+
+    @Value("${security.endpoint.alumnos}")
+    private String alumnosEndpoint;
+
+    @Value("${security.endpoint.idiomas}")
+    private String idiomasEndpoint;
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -42,42 +50,42 @@ public class SecurityConfig {
         return config.getAuthenticationManager();
     }
 
-    // @Bean
-    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
-    // Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
+    Exception {
 
-    // http.authorizeHttpRequests((requests) -> requests
-    // .requestMatchers("/idiomas/**").hasRole("ADMIN")
-    // .requestMatchers("/alumnos/**").hasAnyRole("USER", "ADMIN"));
-    // http.formLogin(withDefaults());
-    // http.httpBasic(withDefaults());
-    // return http.build();
+    http.authorizeHttpRequests((requests) -> requests
+    .requestMatchers(idiomasEndpoint).hasRole(Rol.ADMIN.name())
+    .requestMatchers(alumnosEndpoint).hasAnyRole(Rol.ADMIN.name()));
+    http.formLogin(withDefaults());
+    http.httpBasic(withDefaults());
+    return http.build();
+    }
+
+    // @Bean
+    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    //     http.csrf(csrf -> csrf.disable()) // Desactiva CSRF
+    //             .authorizeHttpRequests(auth -> auth
+    //                     .anyRequest().permitAll() // Permite todas las peticiones
+    //             )
+    //             .formLogin(form -> form.disable()) // Desactiva login
+    //             .httpBasic(basic -> basic.disable()); // Desactiva basic auth
+
+    //     return http.build();
     // }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    // @Bean
+    // public CorsConfigurationSource corsConfigurationSource() {
+    //     CorsConfiguration configuration = new CorsConfiguration();
+    //     configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+    //     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    //     configuration.setAllowedHeaders(Arrays.asList("*"));
+    //     configuration.setAllowCredentials(true);
 
-        http.csrf(csrf -> csrf.disable()) // Desactiva CSRF
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // Permite todas las peticiones
-                )
-                .formLogin(form -> form.disable()) // Desactiva login
-                .httpBasic(basic -> basic.disable()); // Desactiva basic auth
-
-        return http.build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+    //     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    //     source.registerCorsConfiguration("/**", configuration);
+    //     return source;
+    // }
 
 }
