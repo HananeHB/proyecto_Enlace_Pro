39e13d44f308ba84d4793c66a26f2a4a51b1ef64
HananeHB <hananebh146@gmail.com>
2026-02-25 23:26:00 +0100

    configuración para la bbdd hecha

--- src/main/java/es/etg/daw/dawes/java/rest/enlacePro/enlacePro/auth/infraestructure/db/jpa/entity/UserEntity.java
+++ src/main/java/es/etg/daw/dawes/java/rest/enlacePro/enlacePro/auth/infraestructure/db/jpa/entity/UserEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -20,7 +21,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "usuarios")
 public class UserEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String firstname;
     private String lastname;
