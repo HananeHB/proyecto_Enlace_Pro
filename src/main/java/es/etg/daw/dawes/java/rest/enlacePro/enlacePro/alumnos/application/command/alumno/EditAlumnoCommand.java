185661af14fb094e72659c70d9cf7925c4190585
HananeHB <hananebh146@gmail.com>
2026-02-26 00:25:54 +0100

    arreglos en las clases (borrados los imports no usados en las clases)

--- src/main/java/es/etg/daw/dawes/java/rest/enlacePro/enlacePro/alumnos/application/command/alumno/EditAlumnoCommand.java
+++ src/main/java/es/etg/daw/dawes/java/rest/enlacePro/enlacePro/alumnos/application/command/alumno/EditAlumnoCommand.java
@@ -1,6 +1,5 @@
 package es.etg.daw.dawes.java.rest.enlacePro.enlacePro.alumnos.application.command.alumno;
 
-import es.etg.daw.dawes.java.rest.enlacePro.enlacePro.alumnos.domain.model.Idioma;
 import es.etg.daw.dawes.java.rest.enlacePro.enlacePro.alumnos.domain.model.id.AlumnoId;
 import es.etg.daw.dawes.java.rest.enlacePro.enlacePro.alumnos.domain.model.id.IdiomaId;
 import lombok.AllArgsConstructor;
